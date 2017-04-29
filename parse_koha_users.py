ae7b096a5c163bf0d6b06bf4ec3f0222d42c0f15
allsway <ssinick@calstate.edu>
2017-04-28 18:15:07 -0700

    Created brief script to parse csv file into .dat file

---
+++ parse_koha_users.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import sys
+import csv
+import datetime
+
+# turns user names into user ID
+def get_user_id(row):
+    first_init = ''
+    if len(row[1]) > 0:
+        first_init = row[1].strip(' ')[0].lower()
+    second = row[0].strip()
+    return first_init + second
+    #print (first_init + second)
+
+def get_date():
+    day = datetime.datetime.today()
+    return day.strftime('%Y%m%d%H%m')
+
+
+# read file and output offline circ file
+def parse_row(row):
+    # 201701231123
+    transaction_date = get_date()
+    transaction = 'L'
+    user_id = get_user_id(row)
+    barcode = row[5]
+    spaces = '                                                                       '
+    print (transaction_date + transaction + barcode + spaces + user_id)
+
+
+
+def read_loans(loans):
+	f  = open(loans,'rt')
+	try:
+		reader = csv.reader(f)
+		header = next(reader)
+		for row in reader:
+			parse_row(row)
+	finally:
+		f.close()
+
+loans = sys.argv[1]
+read_loans(loans)
